f561195882c3c582f07820fb2b00bd2ec84930bb
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-06-27 14:28:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -523,7 +523,7 @@ if selected_season:
                 line_group='line_group',
                 color='color',
                 color_discrete_map={
-                    'court': '#fdfbf5'',
+                    'court': '#fdfbf5',
                     'hoop': '#e47041'
                 }
             )
